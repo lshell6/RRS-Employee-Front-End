f0b970956fcb3e07bea9e62d4cd332b40a152d91
Kaboateng <100966192+Kaboateng@users.noreply.github.com>
2022-08-17 14:19:56 -0400

    Update cart.component.ts

--- src/app/component/cart/cart.component.ts
+++ src/app/component/cart/cart.component.ts
@@ -13,11 +13,14 @@ export class CartComponent implements OnInit {
 
   redeemForm: FormGroup;
   cart: Cart;
+  carts: Cart[];
   msg: string;
+  employee: Employee;
   constructor(private cartService: CartService, private router: Router) { }
 
   ngOnInit(): void {
     this.msg = '';
+    this.cart.total = this.carts[0].point_value + this.carts[1].point_value + this.carts[2].point_value;
     this.redeemForm = new FormGroup({
       name: new FormControl(''),
       ptValue: new FormControl('')
@@ -28,10 +31,11 @@ export class CartComponent implements OnInit {
     this.cart = this.redeemForm.value;
     this.cartService.redeemItems(this.cart).subscribe({
       next: ()=>{
-        this.msg = 'Item(s) redeemed'
+        this.employee.total_points = this.employee.current_points - this.cart.total;
         let cartArr = this.cartService.cart$.getValue();
         cartArr.push(this.cart);
         this.cartService.cart$.next(cartArr);
+        this.msg = 'Item(s) redeemed';
         this.router.navigateByUrl('/dashboard');
       },
       error: (e)=>{
